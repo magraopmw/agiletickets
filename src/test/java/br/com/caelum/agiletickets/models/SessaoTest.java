a25b6c67c887df20bf629fa3559620deb02b864e
Eduardo <eduardo@tre-to.jus.br>
2013-11-12 09:03:31 -0300

    Inclusão de cenário de testes: reservar total disponível.

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -30,4 +30,11 @@ public class SessaoTest {
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
+	
+	@Test
+	public void reservarMaximoDisponivel() throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(5);
+        Assert.assertTrue(sessao.podeReservar(5));
+	}
 }
